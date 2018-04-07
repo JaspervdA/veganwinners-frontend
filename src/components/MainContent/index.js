18beb3018a2b7d36c60144730a51397d858c70c2
missEnergy <everseput@hotmail.com>
2018-04-07 22:35:20 +0200

    typos

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -37,7 +37,10 @@ class MainContent extends React.Component {
         </Toast>
           <Section style={{padding:"20px"}}>
             <Headline>
-              Welkom op Veganwinners!
+              Welkom op Veganwinners
+            </Headline>
+            <Headline size="small">
+              <i>Be the change you wish to see in the world...</i>
             </Headline>
             Deze website is bedoeld voor iedereen die ook houdt van lekker eten, maar dan wel vega en vegan. <br/><br/>
             Sinds 2 jaar verzamelen wij, Jasper en Ellissa, gemakkelijke en toegankelijke recepten zonder vlees en vaak ook zonder zuivel. Zo proberen wij onze milieu-impact een klein beetje te reduceren. <br/><br/>
@@ -46,7 +49,7 @@ class MainContent extends React.Component {
           <RecipeList {...props} data={{recipes:this.state.recipes, isLoading:this.state.isLoading}}/>
           <Section style={{padding:"20px"}}>
             <p> Voor vragen en op- of aanmerkingen kun je ons <a href="mailto:ellissa@veganwinners.com">mailen</a>.<br/><br/>
-            Verder is onze website is volledig open source. Je kunt onze <a href="https://github.com/JaspervdA/veganwinners-frontend">frontend</a> en <a href="https://github.com/missEnergy/veganwinners-backend/">backend</a> vinden op Github. Wij staan open voor feedback en feature request!</p>
+            Verder is onze website volledig open source. Je kunt onze <a href="https://github.com/JaspervdA/veganwinners-frontend">frontend</a> en <a href="https://github.com/missEnergy/veganwinners-backend/">backend</a> vinden op Github. Wij staan open voor feedback en feature request!</p>
           </Section>
         </div>
       )}/>
